018f7781bc94e723a3e26a327e2560e910c55422
Daniil Pinchuk <JeetWork168@gmail.com>
2024-01-08 22:12:38 +0300

    change portfolio section,and use swiper.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Получаем все элементы фильтра
 var filters = document.querySelectorAll('.portfolio-filter li');
 
+const swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // mousewheel: true,
+    keyboard: true,
+});
+
 // Добавляем обработчик события для каждого элемента фильтра
 filters.forEach(function (filter) {
     filter.addEventListener('click', function () {
